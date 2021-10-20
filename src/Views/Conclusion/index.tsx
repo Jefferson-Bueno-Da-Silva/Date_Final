1a638ab08e49cb6f7c431af63dd1cf5505847483
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-10-19 21:55:01 -0300

    create personality screen

--- src/Views/Conclusion/index.tsx
+++ src/Views/Conclusion/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
+import { Image } from "react-native";
 
 import { useNavigation } from "@react-navigation/core";
 type ScreenProp = NativeStackNavigationProp<RootStackParamList, "Home">;
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../routes";
 
-import { Header } from "../../Components";
-
-import { Container } from "./styles";
+import { Header, NextButton } from "../../Components";
+import { Body, Container, Title } from "./styles";
 const image = require("../../assets/bubble_back.png");
 
 export const Conclusion: React.FC = () => {
@@ -15,6 +15,15 @@ export const Conclusion: React.FC = () => {
   return (
     <Container source={image} resizeMode="cover">
       <Header title="personality" onPress={goBack} />
+      <Body>
+        <Title>your personality is adventurous</Title>
+        <Image
+          source={require("../../assets/adventurer.png")}
+          style={{ height: 200, width: 200 }}
+          resizeMode="center"
+        />
+        <NextButton text="Start" />
+      </Body>
     </Container>
   );
 };
--- src/Views/Conclusion/styles.ts
+++ src/Views/Conclusion/styles.ts
@@ -4,3 +4,21 @@ export const Container = styled.ImageBackground`
   flex: 1;
   background-color: #000;
 `;
+
+export const Body = styled.View`
+  flex: 1;
+  padding: 28px;
+  justify-content: space-evenly;
+`;
+
+export const Title = styled.Text`
+  font-weight: bold;
+  font-size: 30px;
+  line-height: 40px;
+
+  align-items: center;
+  text-align: center;
+
+  color: #ffffff;
+  text-shadow: 1px 4px 4px rgba(0, 0, 0, 0.25);
+`;
